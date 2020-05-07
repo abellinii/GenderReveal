7990dc8f88981c259fd185220373b260cb141a66
The Passive Trust <thepassivetrust@Wades-MacBook-Pro-2.local>
2020-05-06 19:39:37 -0700

    Updates

--- register-service-worker.js
+++ register-service-worker.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker
-      .register('/abellinii/GenderReveal/expo-service-worker.js', { scope: '/abellinii/GenderReveal/' })
+      .register('/GenderReveal/expo-service-worker.js', { scope: '/GenderReveal/' })
       .then(function(info) {
         // console.info('Registered service-worker', info);
       })
